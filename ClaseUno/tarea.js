f55de70ca1c1c951400ed9e8704cb2bd9fa559ff
Lemon <lisandro.m.yo@gmail.com>
2023-02-27 12:54:33 -0300

    Primer commit

---
+++ ClaseUno/tarea.js
@@ -0,0 +1,28 @@
+let auto = "Corolla";
+
+console.log(auto);
+
+console.log(typeof(auto));
+
+auto=308;
+
+console.log(auto);
+console.log(typeof(auto));
+
+auto="C4";
+
+console.log(auto);
+console.log(typeof(auto));
+
+let nombre = "Lisandro";
+
+let apellido = "Martin";
+
+console.log(nombre,apellido);
+
+let fruta = "naranja";
+
+let tipoDeLaVariableFruta = typeof(fruta);
+
+console.log(tipoDeLaVariableFruta);
+
